04123db2439d640e33e1f041c51812e978049f40
Weerawat Tarujettanarom <weerawat.36@gmail.com>
2017-11-24 17:08:26 +0700

    First commit.

--- MPA3/Program.cs
+++ MPA3/Program.cs
@@ -9,6 +9,34 @@ namespace MPA3
     {
         static void Main(string[] args)
         {
+            if(args == null || args.Length == 0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Arguments not specified completely!");
+                Console.WriteLine("");
+                Console.WriteLine("Press any key to close this window");
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+                //Console.WriteLine("");
+                //Console.WriteLine(args[0]);
+                //Console.ReadKey();
+                try
+                {
+                    System.Diagnostics.Process.Start("java.exe", @"-jar GenXML.jar d:\genxml\json_input.json d:\express\expressi\etaxinvoice\xml\EtDa-invoice.xml");
+                    Console.WriteLine("");
+                    Console.WriteLine("Success");
+                    Console.ReadKey();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
