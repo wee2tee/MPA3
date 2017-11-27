891c91454f2c9bdd66b88185b06d84bfc3481d2b
Weerawat Tarujettanarom <weerawat.36@gmail.com>
2017-11-27 17:26:22 +0700

    adding MkX, MkP

--- MPA3/Program.cs
+++ MPA3/Program.cs
@@ -1,42 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using MkX;
+using MkP;
 
 namespace MPA3
 {
     class Program
     {
+        /**
+         * args[0] = process type "XML" / "PDF"
+         * 
+         * XML_PROCESS
+         * args[1] = path to source json file
+         * args[2] = path to store xml result file
+         * 
+         * PDF_PROCESS
+         * args[1] = path to original pdf file
+         * args[2] = path to xml embeded file
+         * args[3] = path to color profile file
+         * args[4] = path to destination output file
+         * args[5] = document type
+         * args[6] = document file name (named xml file embeded)
+         * args[7] = document version
+         * */
+
+        //private const string XML_PROCESS = "XML";
+        //private const string PDF_PROCESS = "PDF";
+        public enum PROCESS_TYPE
+        {
+            XML,
+            PDF
+        }
+
         static void Main(string[] args)
         {
-            if(args == null || args.Length == 0)
+            if(!(args != null && (args.Length == 3 || args.Length == 8)))
             {
-                Console.WriteLine("");
-                Console.WriteLine("Arguments not specified completely!");
-                Console.WriteLine("");
-                Console.WriteLine("Press any key to close this window");
-                Console.ReadKey();
+                Console.WriteLine("Error : Arguments not specified completely!");
                 return;
             }
             else
             {
-                //Console.WriteLine("");
-                //Console.WriteLine(args[0]);
-                //Console.ReadKey();
+                if(!(args[0].Trim() == PROCESS_TYPE.XML.ToString() || args[0].Trim() == PROCESS_TYPE.PDF.ToString()))
+                {
+                    Console.WriteLine("Error : Process type is incorrect!");
+                    return;
+                }
+
                 try
                 {
-                    System.Diagnostics.Process.Start("java.exe", @"-jar GenXML.jar d:\genxml\json_input.json d:\express\expressi\etaxinvoice\xml\EtDa-invoice.xml");
-                    Console.WriteLine("");
-                    Console.WriteLine("Success");
-                    Console.ReadKey();
+                    if(args[0].Trim() == PROCESS_TYPE.XML.ToString())
+                    {
+                        string json_path = args[1];
+                        string dest_xml = args[2];
+
+                        XmlDoc xml = new XmlDoc(json_path, dest_xml);
+                        var result = xml.CreateXml();
+
+                        if (result.createSuccess)
+                        {
+                            Console.WriteLine("OK");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fail! , " + result.message);
+                        }
+                        return;
+                    }
+
+                    if(args[0].Trim() == PROCESS_TYPE.PDF.ToString())
+                    {
+                        string pdf_original_file_path = args[1];
+                        string xml_embeded_file_path = args[2];
+                        string color_profile_file_path = args[3];
+                        string destination_pdf_file_path = args[4];
+                        string document_type = args[5];
+                        string document_file_name = args[6];
+                        string document_version = args[7];
+
+                        PdfDoc pdf = new PdfDoc(pdf_original_file_path, xml_embeded_file_path, color_profile_file_path, destination_pdf_file_path, document_type, document_file_name, document_version);
+                        var result = pdf.CreatePdf();
+
+                        if (result.createSuccess)
+                        {
+                            Console.WriteLine("OK");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fail! , " + result.message);
+                        }
+                        return;
+                    }
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("");
-                    Console.WriteLine(ex.Message);
-                    Console.ReadKey();
+                    Console.WriteLine("Error : " + ex.Message);
                 }
             }
         }
+
+        
     }
 }
---
+++ MkX/XmlDoc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace MkX
+{
+    public class XmlDoc
+    {
+        private string json_source_path = null;
+        private string destination_xml_path = null;
+
+        public XmlDoc(string json_source_path = null, string destination_xml_path = null)
+        {
+            this.json_source_path = json_source_path;
+            this.destination_xml_path = destination_xml_path;
+        }
+
+        public CreateXmlResult CreateXml()
+        {
+            string command = "java";
+            string arguments = "-jar " + AppDomain.CurrentDomain.BaseDirectory + "GenXML.jar " + this.json_source_path + " " + this.destination_xml_path;
+
+            Process process = new Process();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.FileName = command;
+            process.StartInfo.Arguments = arguments;
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            if (output.ToLower().Contains("success"))
+            {
+                return new CreateXmlResult { createSuccess = true, message = output };
+            }
+            else
+            {
+                return new CreateXmlResult { createSuccess = false, message = output };
+            }
+        }
+
+    }
+
+    public class CreateXmlResult
+    {
+        public bool createSuccess { get; set; }
+        public string message { get; set; }
+    }
+}
